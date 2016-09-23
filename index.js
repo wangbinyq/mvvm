84476b743d7845415412d6e7dbc1312c4daf2885
wangbin <wangbin_ydkf@kedacom.com>
2016-09-23 10:23:35 +0800

    Observer add array, keypath add each binder

--- index.js
+++ index.js
@@ -4,59 +4,169 @@ function defined(obj) {
     return !_.isUndefined(obj) && !_.isNull(obj)
 }
 
-class Observer {
-    constructor(obj, key, cb) {
-        this.obj = obj
-        this.key = key
-        this.cb = cb
-        this.obj.$$callbacks = this.obj.$$callbacks || {}
-        this.obj.$$callbacks[this.key] = this.obj.$$callbacks[this.key] || []
+const ARRAY_METHODS = [
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'sort',
+    'reverse',
+    'splice'
+]
 
-        this.observe()
+class Observer {
+    constructor(obj, keypath, callback) {
+        this.obj = obj
+        this.keypath = keypath
+        this.callback = callback
+        this.objectPath = []
+        this.update = this.update.bind(this)
+
+        this.tokenize()
+
+        if(this.key && _.isObject(this.target = this.realize())) {
+            this.observe(this.key, this.target, this.callback)
+        }
     }
 
-    observe() {
-        const observer = this
-        const obj = observer.obj
-        const key = observer.key
-        const callbacks = obj.$$callbacks[key] 
-        let value = obj[key]
+    tokenize() {
+        this.tokens = this.keypath.split('.')
+        this.key = this.tokens.pop()
+    }
 
-        const desc = Object.getOwnPropertyDescriptor(obj, key)
-        if(!(desc && (desc.get || desc.set))) {
-            Object.defineProperty(obj, key, {
+    observeMutations(obj, key) {
+        let mutations = obj[key]
+        if(!_.isArray(mutations)) {
+            return
+        }
+        ARRAY_METHODS.forEach((method) => {
+            mutations[method] = (...args) => {
+                Array.prototype[method].apply(mutations, args)
+                if(obj.$callbacks[key]) {
+                    obj.$callbacks[key].forEach((cb) => {
+                        cb()
+                    })
+                }
+            }
+        })
+    }
+
+    realize() {
+        let current = this.obj, unreached = false, prev
+        this.tokens.forEach((token, index) => {
+            if(_.isObject(current)) {
+                if(typeof this.objectPath[index] !== 'undefined') {
+                    if(current !== (prev = this.objectPath[index])) {
+                        this.unobserve(token, prev, this.update)
+                        this.observe(token, current, this.update)
+                        this.objectPath[index] = current
+                    }
+                } else {
+                    this.observe(token, current, this.update)
+                    this.objectPath[index] = current
+                }
+                current = current[token]
+            } else {
+                if(unreached === false) {
+                    unreached = index
+                }
+
+                if(prev = this.objectPath[index]) {
+                    this.unobserve(token, prev, this.update)
+                }
+            }
+        })
+
+        if(unreached !== false) {
+            this.objectPath.splice(unreached)
+        }
+
+        return current
+    }
+
+    update() {
+        let next, oldValue
+        if((next = this.realize()) !== this.target) {
+            if(_.isObject(this.target)) {
+                this.unobserve(this.key, this.target, this.callback)
+            }
+
+            if(_.isObject(next)) {
+                this.observe(this.key, next, this.callback)
+            }
+
+            oldValue = this.value
+            this.target = next
+            if (_.isFunction(this.value) || this.value !== oldValue) {
+                this.callback()
+            }
+        }
+    }
+
+    observe(key, target, callback) {
+        target.$callbacks = target.$callbacks || {}
+        target.$callbacks[key] = target.$callbacks[key] || []
+        let desc = Object.getOwnPropertyDescriptor(target, key)
+
+        if (!(desc && (desc.get || desc.set))) {
+            let value = target[key]
+            const observer = this
+            Object.defineProperty(target, key, {
                 get() {
                     return value
                 },
                 set(newValue) {
-                    if(value !== newValue) {
+                    if(newValue !== value) {
                         value = newValue
-                        
-                        callbacks.forEach((cb) => {
-                            cb()
-                        })
+                        if(target.$callbacks[key]) {
+                            target.$callbacks[key].forEach((cb) => {
+                                cb()
+                            })
+                        }
+                        observer.observeMutations(target, key)
                     }
                 }
             })
         }
-        if(callbacks.indexOf(observer.cb) === -1) {
-            callbacks.push(observer.cb)
+
+        if(target.$callbacks[key].indexOf(callback) === -1) {
+            target.$callbacks[key].push(callback)
         }
+        this.observeMutations(target, key)
     }
 
-    unobserve() {
-        if(defined(this.obj.$$callbacks[this.key])) {
-            const index = this.obj.$$callbacks[this.key].indexOf(this.cb)
-            this.obj.$$callbacks[this.key].splice(index, 1)
+    unobserve(key, target, callback) {
+        if(key === undefined) {
+            let obj
+            this.tokens.forEach((token, index) => {
+                if(obj = this.objectPath[index]) {
+                    this.unobserve(token, obj, this.update)
+                }
+            })
+            if(_.isObject(this.target)) {
+                this.unobserve(this.key, this.target, this.callback)
+            }
+        } else {
+            if(target.$callbacks && target.$callbacks[key]) {
+                const index = target.$callbacks[key].indexOf(callback)
+                if(index !== -1) {
+                    target.$callbacks[key].splice(index, 1)
+                }
+            }
         }
     }
 
     get value() {
-        return this.obj[this.key]
+        if(_.isObject(this.target)) {
+            return this.target[this.key]
+        }
+        
     }
 
     set value(newValue) {
-        this.obj[this.key] = newValue
+        if(_.isObject(this.target)) {
+            this.target[this.key] = newValue
+        }     
     }
 }
 
@@ -142,8 +252,12 @@ class ViewModel {
             }
 
             if(defined(binder)) {
+                if(binder.block) {
+                    block = true
+                }
                 this.bindings.push(new Binding(this, el, key, binder))
             }
+            el.removeAttribute(`data-${data}`)
         }
 
         if(!block) {
@@ -165,7 +279,7 @@ class ViewModel {
     }
 
     unbind() {
-        this.bindins.forEach(binding => {
+        this.bindings.forEach(binding => {
             binding.unbind()
         })
     }
@@ -208,7 +322,55 @@ ViewModel.binders = {
     },
 
     each: {
-        block: true
+        block: true,
+
+        bind(el) {
+            if(!defined(this.marker)) {
+                let attr = this.args[0]
+                this.marker = document.createComment(`mvvm - each - ${attr}`)
+                this.iterated = []
+
+                el.removeAttribute('data-each')
+                el.parentNode.insertBefore(this.marker, el)
+                el.parentNode.removeChild(el)
+            } else {
+                this.iterated.forEach((vm) => {
+                    vm.bind()
+                })
+            }
+        },
+
+        unbind(el) {
+            if(defined(this.iterated)) {
+                this.iterated.forEach((vm) => {
+                    vm.unbind()
+                })
+                this.iterated = []
+            }
+        },
+
+        sync(el, value) {
+            let item = this.args[0]
+            let collection = value || []
+            
+            this.iterated.forEach((vm) => {
+                vm.unbind()
+                this.marker.parentNode.removeChild(vm.el)
+            })
+            this.iterated = []
+
+            collection.forEach((model, $index) => {
+                let data = {
+                    $index,
+                    [item]: model,
+                    $root: this.vm.model
+                }
+                let template = el.cloneNode(true)
+                let vm = new ViewModel(template, data)
+                this.iterated.push(vm)
+                this.marker.parentNode.appendChild(template)
+            })
+        }
     },
 
     on: {
@@ -234,6 +396,16 @@ const obj = {
     show: false,
     reverse() {
         obj.text = obj.text.split('').reverse().join('')
+    },
+    obj: {
+        arr: [
+            'test1',
+            'test2',
+            'test3'
+        ]
+    },
+    add() {
+        obj.obj.arr.push(obj.text)
     }
 }
 const ob = new Observer(obj, 'a', () => { 
--- readme.md
+++ readme.md
@@ -5,5 +5,24 @@
 一个简单的带有双向绑定的 MVVM 实现.
 [例子](https://445141126.github.io/mvvm/)
 
-本实现主要参考 [rivets.js 的 es6 分支](https://github.com/mikeric/rivets/tree/es6), 其中 Observer 类是参考 [adapter.js](https://github.com/mikeric/rivets/blob/es6/src/adapter.js) 实现, 但是没有实现 keypath, 也就是说只能 observe 对象的属性, 而不能 observe 子对象的属性.
+
+## 使用
+新建一个 ViewModel 对象, 参数分别为 DOM 元素以及绑定的数据即可.
+
+## 指令
+
+本 MVVM 的指令使用 data 数据, 即 data-html = "text" 表示这个 DOM 元素的 innerHTMl 为 model 中的 text 属性.
+对某些指令还可以添加参数, 比如 data-on="reverse:click", 表示 DOM 元素添加 click 事件, 处理函数为 model 中的 reverse 属性.
+
+- value: 可以在 input 中使用, 只对 checkbox 进行特殊处理
+- text, html: 分别修改 innerText 和 innerHTML
+- show: 控制指定元素显示与否
+- each: 循环 DOM 元素, 每个元素绑定新的 ViewModel, 通过 $index 可以获取当前索引, $root 表示根 ViewModel 的属性
+- on: 绑定事件,
+- *: 绑定特定属性
+
+## 参考
+本实现主要参考 [rivets.js 的 es6 分支](https://github.com/mikeric/rivets/tree/es6), 其中 Observer 类是参考 [adapter.js](https://github.com/mikeric/rivets/blob/es6/src/adapter.js) 实现.
 Binding 就是 [bindings.js](https://github.com/mikeric/rivets/blob/es6/src/bindings.js) 对应的简化, 相当于其他 MVVM 中指令, ViewModel 对应 [view.js](https://github.com/mikeric/rivets/blob/es6/src/view.js).
+
+>> PS: 由于双向绑定只是简单的实现, 因此指令中的
